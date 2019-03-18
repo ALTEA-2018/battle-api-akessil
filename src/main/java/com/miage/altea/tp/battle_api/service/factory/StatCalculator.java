260da5772bb889857f681090e25b2332b631ee4a
ouikene koceila <koceila.ouikene@worldline.com>
2019-03-18 13:22:48 +0100

    Implement calculate speed defense hp and damage

--- src/main/java/com/miage/altea/tp/battle_api/service/factory/StatCalculator.java
+++ src/main/java/com/miage/altea/tp/battle_api/service/factory/StatCalculator.java
@@ -9,7 +9,35 @@ public class StatCalculator {
     }
 
     public int calculateAttack(int level, int baseAttack){
-        int attack = 5 + (baseAttack * level/50);
-        return attack;
+        return calculateStat(level, baseAttack);
+    }
+    public int calculateSpeed(int level, int baseSpeed){
+        return calculateStat(level, baseSpeed);
+    }
+
+    public int calculateDefense(int level, int baseDefense){
+        return calculateStat(level, baseDefense);
+    }
+
+    public int calculateHP(int level, int baseHP){
+        int hp = 10 + level + (baseHP*level/50);
+        return hp;
+    }
+
+    /**
+     *
+     * @param level: trainer pokemon level
+     * @param attack: trainer pokemon attack
+     * @param defense: opponent pokemon defense
+     * @return
+     */
+    public int calculateDamage(int level, int attack, int defense){
+        int damage = (int)( (2 * (float)level/5) + (2 * (((float)attack/defense))) )+2;
+        return damage;
+    }
+
+    private int calculateStat(int level, int baseStat){
+        int stat = 5 + (baseStat * level/50);
+        return stat;
     }
 }
--- src/test/java/com/miage/altea/tp/battle_api/service/factory/StatCalculatorTest.java
+++ src/test/java/com/miage/altea/tp/battle_api/service/factory/StatCalculatorTest.java
@@ -13,4 +13,32 @@ public class StatCalculatorTest {
         var expectedAttack = 11;
         assertEquals(expectedAttack, attack );
     }
+
+    @Test
+    public void calculateDefenseTest(){
+        var defense= calculator.calculateDefense(6,40);
+        var expectedDefense = 9;
+        assertEquals(expectedDefense, defense );
+    }
+
+    @Test
+    public void calculateSpeedTest(){
+        var speed= calculator.calculateSpeed(6,90);
+        var expectedSpeed = 15;
+        assertEquals(expectedSpeed, speed );
+    }
+
+    @Test
+    public void calculateHPTest(){
+        var hp= calculator.calculateHP(6,35);
+        var expectedHP = 20;
+        assertEquals(expectedHP, hp );
+    }
+
+    @Test
+    public void calculateDamageTest(){
+        var damage= calculator.calculateDamage(6,15, 10);
+        var expectedDamage = 7;
+        assertEquals(expectedDamage, damage );
+    }
 }
