567f2cef0e3e7e62781fd779fb53d30c268465da
ouikene koceila <koceila.ouikene@worldline.com>
2019-04-06 20:18:38 +0200

    Delete security from battle

--- src/main/java/com/miage/altea/tp/battle_api/config/RestConfiguration.java
+++ src/main/java/com/miage/altea/tp/battle_api/config/RestConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
